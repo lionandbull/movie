64182079ffb40ef7219750ce8bd53041a75bd742
Wade <liuweixi0819@gmail.com>
2019-05-06 13:22:22 -0400

    change name

---
+++ precache-manifest.2b05e9777d6298cb3c61e1905db60d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8d8bcb0314d450539ee",
+    "url": "/movie-react/static/js/runtime~main.72cbdd88.js"
+  },
+  {
+    "revision": "b4892dee32469db199d1",
+    "url": "/movie-react/static/js/main.a504cf63.chunk.js"
+  },
+  {
+    "revision": "fa87c385030b80738559",
+    "url": "/movie-react/static/js/2.0d63f307.chunk.js"
+  },
+  {
+    "revision": "b4892dee32469db199d1",
+    "url": "/movie-react/static/css/main.86977d58.chunk.css"
+  },
+  {
+    "revision": "fa87c385030b80738559",
+    "url": "/movie-react/static/css/2.f249d030.chunk.css"
+  },
+  {
+    "revision": "5810f2c6750b9657cb78a60363283c33",
+    "url": "/movie-react/index.html"
+  }
+];
